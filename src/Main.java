294d829704086f33c67d0a0ed9aa2eacbac95288
ShmyhelskyiAndriy <126695567+ShmyhelskyiAndriy@users.noreply.github.com>
2024-04-30 21:17:42 +0300

    Final? :_( №3

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,48 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        int [] array = new int[100];
-        int [] outArray = new int[100];
-        Util.filling(array);
+        int [] array = new int[10];
 
+        Util.filling(array);
+//        Util.filling(outArray);
+        int [] outArray = Arrays.copyOf(array,array.length);
+
+//        System.out.println(Arrays.toString(array));
+//        System.out.println(Arrays.toString(outArray));
+
+
+        long time = System.currentTimeMillis();
+        QuickSort.quickSortMid(array,1,10);
+        System.out.println("Quick:" + (System.currentTimeMillis() - time));
         System.out.println(Arrays.toString(array));
 
+
+
+
+
+
+
+
+//        System.out.println(Arrays.toString(CountingSort.countSortWithNegatives(array)));
+
+
+//        SelectionSort.selecSort(array);
+//
+//        BubbleSort.bubbleSort(outArray);
+
+//        time = System.currentTimeMillis();
+//        QuickSort.quickSortMid(array, 0, array.length - 1);
+////        System.out.println(Arrays.toString(array));
+//        System.out.println("mid:" + (System.currentTimeMillis() - time));
+//
+//
 //        long time = System.currentTimeMillis();
-//        MergeSort.mergeSort(array);
-//        System.out.println(System.currentTimeMillis() - time);
-//
-//        System.out.println(Arrays.toString(array));
-
-
-//        RadixSort.radixSort(array);
-//        System.out.println(Arrays.toString(array));
-//
-//        QuickSort.quickSortNeg(array, 0, array.length - 1);
-//        System.out.println(Arrays.toString(array));
+//        QuickSort.quickSortMid(outArray, 0, array.length - 1);
+//        System.out.println(Arrays.toString(outArray));
+//        System.out.println("rand:" + (System.currentTimeMillis() - time));
 
 //        System.out.println(Arrays.toString(CountingSort.countSortWithNegatives(array)));
 
