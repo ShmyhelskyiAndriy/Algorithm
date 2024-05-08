7e0761889ba08265c2b3d87f5e8871ebae93a6f1
ShmyhelskyiAndriy <126695567+ShmyhelskyiAndriy@users.noreply.github.com>
2024-05-09 00:08:46 +0300

    Final? :_( №6

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,49 @@
-
-
-
-
-import java.util.Arrays;
-
-import java.util.Queue;
-
 public class Main {
     public static void main(String[] args) {
 
-//        int [] array = new int[100];
-//
-//        Util.filling(array);
-//
-//        System.out.println("Unsorted array:");
+        int [] array = new int[1000000];
+
+        Util.filling(array);
+
+        System.out.println("Unsorted array:");
 //        System.out.println(Arrays.toString(array.clone()));
-//
-//        long time;
-//
-//        // Сортування з опорою на останній елемент
-//        time = System.currentTimeMillis();
-//        int [] arrayLast = array.clone();
-//        QuickSort.quickSortLast(arrayLast, 0, array.length - 1);
-//        System.out.println("\nСортування з опорою на останній елемент: \n" + "час" + (System.currentTimeMillis() - time));
+
+        long time;
+
+        // Сортування з опорою на останній елемент
+        time = System.currentTimeMillis();
+        int [] arrayLast = array.clone();
+        QuickSort.quickSortLast(arrayLast, 0, array.length - 1);
+        System.out.println("\nСортування з опорою на останній елемент: \n" + "час" + (System.currentTimeMillis() - time));
 //        System.out.println(Arrays.toString(arrayLast));
-//
-//
-//        // Сортування з опорою на середній елемент
-//        time = System.currentTimeMillis();
-//        int [] arrayMid = array.clone();
-//        QuickSort.quickSortMid(arrayMid, 0, array.length - 1);
-//        System.out.println("\nСортування з опорою на середній елемент: \n" + "час" + (System.currentTimeMillis() - time));
+
+
+        // Сортування з опорою на середній елемент
+        time = System.currentTimeMillis();
+        int [] arrayMid = array.clone();
+        QuickSort.quickSortMid(arrayMid, 0, array.length - 1);
+        System.out.println("\nСортування з опорою на середній елемент: \n" + "час" + (System.currentTimeMillis() - time));
 //        System.out.println(Arrays.toString(arrayMid));
-//
-//        // Сортування з опорою на випадковий елемент
-//        time = System.currentTimeMillis();
-//        int [] arrayRand = array.clone();
-//        QuickSort.quickSortRandom(arrayRand, 0, array.length - 1);
-//        System.out.println("\nСортування з опорою на випадковий елементт: \n" + "час" + (System.currentTimeMillis() - time));
+
+        // Сортування з опорою на випадковий елемент
+        time = System.currentTimeMillis();
+        int [] arrayRand = array.clone();
+        QuickSort.quickSortRandom(arrayRand, 0, array.length - 1);
+        System.out.println("\nСортування з опорою на випадковий елементт: \n" + "час" + (System.currentTimeMillis() - time));
 //        System.out.println(Arrays.toString(arrayRand));
 
+
+
+//        Queue<Integer> queue = new Queue<>();
+//        queue.enqueue(40);
+//        queue.enqueue(30);
+//        queue.enqueue(10);
+//        queue.enqueue(50);
+//        queue.show();
+//        queue.dequeue();
+//        queue.show();
+//
+//
 
 
 
@@ -65,25 +69,25 @@ public class Main {
 
 
 
-        Tree<Integer> tree = new Tree<>();
-
-        tree.insertElement(10);
-        tree.insertElement(4);
-        tree.insertElement(16);
-        tree.insertElement(8);
-        tree.insertElement(6);
-        tree.insertElement(1);
-        tree.insertElement(3);
-        tree.insertElement(2);
-        tree.insertElement(12);
-        tree.insertElement(0);
-        tree.insertElement(20);
-        tree.insertElement(30);
-        tree.insertElement(11);
-        tree.insertElement(13);
-        tree.printTree();
-        tree.deleteNode(10);
-        tree.printTree();
+//        Tree<Integer> tree = new Tree<>();
+//
+//        tree.insertElement(10);
+//        tree.insertElement(4);
+//        tree.insertElement(16);
+//        tree.insertElement(8);
+//        tree.insertElement(6);
+//        tree.insertElement(1);
+//        tree.insertElement(3);
+//        tree.insertElement(2);
+//        tree.insertElement(12);
+//        tree.insertElement(0);
+//        tree.insertElement(20);
+//        tree.insertElement(30);
+//        tree.insertElement(11);
+//        tree.insertElement(13);
+//        tree.printTree();
+//        tree.deleteNode(10);
+//        tree.printTree();
     }
 
 
